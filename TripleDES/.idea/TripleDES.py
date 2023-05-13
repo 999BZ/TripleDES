18915073c39017ccdc2dbf1c3eca389b788f2095
999BZ <blendizeqiri@hotmail.com>
2023-05-13 19:00:31 +0200

    Created window and essentials

--- TripleDES/.idea/TripleDES.py
+++ TripleDES/.idea/TripleDES.py
@@ -1,17 +1,43 @@
-import urllib.request
-from bs4 import BeautifulSoup
-import webbrowser
+import tkinter as tk
+from tkinter import filedialog, messagebox
+import os
 
-# specify the URL of the HTML file
-url = 'file:///C:/FIEK/V2 S2/Data Security/TripleDES/TripleDES/.idea/index.html'
+class TripleDesGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Triple DES Encryption/Decryption")
+        self.master.geometry("900x700".format())
 
-# open the URL and read the HTML content
-html = urllib.request.urlopen(url).read()
+        width = self.master.winfo_screenwidth()
+        height = self.master.winfo_screenheight()
+        x = (width - 900) // 2
+        y = (height - 700) // 2
+        self.master.geometry(f"900x700+{x}+{y}")
 
-# create a BeautifulSoup object to parse the HTML
-soup = BeautifulSoup(html, 'html.parser')
+        self.master.configure(background='white')
 
-# extract the information you need
-title = soup.title.string
+        self.key_label = tk.Label(self.master, text="Key:",font=("Montserrat thin", 18),background='white')
+        self.key_label.pack(pady=20)
 
-webbrowser.open_new_tab(url)
+        self.key_entry = tk.Entry(self.master, show="*",font=("Montserrat thin", 18),background='white')
+        self.key_entry.pack()
+
+        self.file_label = tk.Label(self.master, text="Select a file to encrypt/decrypt:",font=("Montserrat thin", 18),background='white')
+        self.file_label.pack(pady=20)
+
+        self.file_button = tk.Button(self.master, text="Browse",font=("Montserrat thin", 14),background='#007fff',foreground='white',activebackground="#0078D7",activeforeground="blue",borderwidth=0.5, relief='flat', padx=10, pady=10, command=self.select_file)
+        self.file_button.pack()
+
+    def select_file(self):
+        #menu for file:
+        self.filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
+        if(self.filename != ""):
+            self.file_label.config(text=self.filename)
+        else:
+            self.file_label.config(text="Select a file to encrypt/decrypt:")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    gui = TripleDesGUI(root)
+    root.mainloop()
